1996105376fedb57b239404589e233572f2a537f
AryanRohilla <aryanrohilla369@gmail.com>
2025-04-03 22:45:32 +0530

    yes commit

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
+console.log("Spotify API");
 let currentSong = new Audio();
-let songs = [
-    "Beretta Amanraj Gill.mp3",
-    "Chaaha Hai Tujhko.mp3",
-    "Dhundle Dhundle - Bunny Johal.mp3",
-    "Dil Tu Jaan Tu Gurnazar.mp3",
-    "Humko Humise Chura Lo.mp3",
-    "Jo Bhi Kasmein Raaz.mp3",
-    "Main Agar Saamne.mp3",
-    "Tumhare_Siva_Full_Song_with_Lyrics_(getmp3.pro).mp3",
-    "Woh Ladki Bahut Yaad Aati  - Qayamat .mp3",
-    "Zaroor – Aparshakti Khurana .mp3"
-];
-let currFolder = "public/songs";
+let songs;
 
 function secondsToMinutesSeconds(seconds) {
     if (isNaN(seconds) || seconds < 0) {
@@ -22,86 +11,105 @@ function secondsToMinutesSeconds(seconds) {
     return `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
 }
 
+async function getSongs() {
+  return [
+    "Beretta Amanraj Gill.mp3",
+    "Chaaha Hai Tujhko.mp3",
+    "Dhundle Dhundle - Bunny Johal.mp3",
+    "Dil Tu Jaan Tu Gurnazar.mp3",
+    "Humko Humise Chura Lo.mp3",
+    "Jo Bhi Kasmein Raaz.mp3",
+    "Main Agar Saamne.mp3",
+    "Tumhare_Siva_Full_Song_with_Lyrics_(getmp3.pro).mp3",
+    "Woh Ladki Bahut Yaad Aati  - Qayamat .mp3",
+    "Zaroor – Aparshakti Khurana .mp3"
+  ];
+}
+
 const playMusic = (track, pause = false) => {
-    currentSong.src = `/${currFolder}/` + track;
+    currentSong.src = "/songs/" + track;
     if (!pause) {
         currentSong.play();
-        document.querySelector("#play").src = "img/pause.svg";
+        play.src = "img/pause.svg";
     }
-    document.querySelector(".songinfo").innerHTML = track;
+    document.querySelector(".songinfo").innerHTML = decodeURI(track);
     document.querySelector(".songtime").innerHTML = "00:00 / 00:00";
 };
 
 async function main() {
-    if (songs.length > 0) {
-        playMusic(songs[0], true);
-    }
-    
+    songs = await getSongs();
+    playMusic(songs[0], true);
+
     let songUL = document.querySelector(".songList ul");
-    songUL.innerHTML = "";
-    
-    for (let song of songs) {
-        songUL.innerHTML += `
-            <li>
-                <img class="invert" src="img/music.svg" alt="">
-                <div class="info">
-                    <div>${song}</div>
-                    <div>Artist</div>
-                </div>
-                <div class="playnow">
-                    <span>Play Now</span>
-                    <img class="invert" src="img/play.svg" alt="">
-                </div>
-            </li>`;
-    }
-    
+    songs.forEach(song => {
+        songUL.innerHTML += `<li>
+            <img class="invert" width="34" src="img/music.svg" alt="">
+            <div class="info">
+                <div>${song.replaceAll("%20", " ")}</div>
+                <div>Punjabi Music</div>
+            </div>
+            <div class="playnow">
+                <span>Play Now</span>
+                <img class="invert" src="img/play.svg" alt="">
+            </div>
+        </li>`;
+    });
+
     document.querySelectorAll(".songList li").forEach((e, index) => {
         e.addEventListener("click", () => {
             playMusic(songs[index]);
         });
     });
-    
-    document.querySelector("#play").addEventListener("click", () => {
+
+    play.addEventListener("click", () => {
         if (currentSong.paused) {
             currentSong.play();
-            document.querySelector("#play").src = "img/pause.svg";
+            play.src = "img/pause.svg";
         } else {
             currentSong.pause();
-            document.querySelector("#play").src = "img/play.svg";
+            play.src = "img/play.svg";
         }
     });
-    
+
     currentSong.addEventListener("timeupdate", () => {
-        document.querySelector(".songtime").innerHTML = 
-            `${secondsToMinutesSeconds(currentSong.currentTime)} / ${secondsToMinutesSeconds(currentSong.duration)}`;
-        document.querySelector(".circle").style.left = 
-            (currentSong.currentTime / currentSong.duration) * 100 + "%";
+        document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds(currentSong.currentTime)}/${secondsToMinutesSeconds(currentSong.duration)}`;
+        document.querySelector(".circle").style.left = Math.min((currentSong.currentTime / currentSong.duration) * 100) + "%";
     });
-    
+
     document.querySelector(".seekbar").addEventListener("click", (e) => {
         let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
         document.querySelector(".circle").style.left = percent + "%";
         currentSong.currentTime = (currentSong.duration * percent) / 100;
     });
-    
+
     document.querySelector(".hamburger").addEventListener("click", () => {
         document.querySelector(".left").style.left = "0";
     });
-    
+
     document.querySelector(".close").addEventListener("click", () => {
-        document.querySelector(".left").style.left = "-120%";
+        document.querySelector(".left").style.left = "-130%";
     });
-    
-    document.querySelector("#previous").addEventListener("click", () => {
-        let index = songs.indexOf(decodeURIComponent(currentSong.src.split("/").pop()));
-        if (index > 0) playMusic(songs[index - 1]);
+
+    // Fix previous button
+    previous.addEventListener("click", () => {
+        currentSong.pause();
+        let currentFilename = decodeURIComponent(currentSong.src.split("/").pop());
+        let index = songs.indexOf(currentFilename);
+        if (index > 0) {
+            playMusic(songs[index - 1]);
+        }
     });
-    
-    document.querySelector("#next").addEventListener("click", () => {
-        let index = songs.indexOf(decodeURIComponent(currentSong.src.split("/").pop()));
-        if (index < songs.length - 1) playMusic(songs[index + 1]);
+
+    // Fix next button
+    next.addEventListener("click", () => {
+        currentSong.pause();
+        let currentFilename = decodeURIComponent(currentSong.src.split("/").pop());
+        let index = songs.indexOf(currentFilename);
+        if (index < songs.length - 1) {
+            playMusic(songs[index + 1]);
+        }
     });
-    
+
     document.querySelector(".range input").addEventListener("change", (e) => {
         currentSong.volume = parseInt(e.target.value) / 100;
         document.querySelector(".volume>img").src = currentSong.volume > 0 ? "img/volume.svg" : "img/mute.svg";
